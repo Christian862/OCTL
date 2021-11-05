cc6593953065866cc5d334e6ad0f3287878b62d0
Christian <ChQu00223@gmail.com>
2021-11-05 10:01:26 -0300

    Port card styles on hover & button text

--- src/componets/Portfolio/PortfolioView.js
+++ src/componets/Portfolio/PortfolioView.js
@@ -6,29 +6,36 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
 class PortfolioView extends React.Component {
   renderProperties() {
     return this.props.properties.map((prop) => (
       <Card key={prop.propertyId} className="prop-card" sx={{ minWidth: 270 }}>
-        <CardContent className="truncate-box">
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            {prop.propertyTitle}
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            adjective
-          </Typography>
-          <Typography className="truncate" variant="body2">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            beatae eos incidunt laboriosam nemo quod ratione sequi similique
-            sunt ullam? Lorem ipsum dolor sit amet, consectetur adipisicing
-            elit. Animi at consequatur dolores, harum ipsa labore natus non odio
-            perferendis sint?
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
+        <CardActionArea>
+          <CardContent className="truncate-box">
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              {prop.propertyTitle}
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              adjective
+            </Typography>
+            <Typography className="truncate" variant="body2">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+              beatae eos incidunt laboriosam nemo quod ratione sequi similique
+              sunt ullam? Lorem ipsum dolor sit amet, consectetur adipisicing
+              elit. Animi at consequatur dolores, harum ipsa labore natus non
+              odio perferendis sint?
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Details</Button>
+          </CardActions>
+        </CardActionArea>
       </Card>
     ));
   }
